5ca820fc5a4e368b4057f2b87a6baea30f407e9e
QCXYZ <100189562+QCXYZ@users.noreply.github.com>
2024-04-13 21:54:29 +0800

    用户管理模块

--- src/main/java/com/bbs/service/UserService.java
+++ src/main/java/com/bbs/service/UserService.java
@@ -4,11 +4,10 @@ import com.bbs.entity.User;
 import com.bbs.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
-@Transactional
+//@Transactional
 @Service
 public class UserService {
     @Resource
